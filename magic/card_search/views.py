874742cd1c6c669cefbb8e8b82d2d8f046d7dc31
jarthurj <jaoldaker1@gmail.com>
2026-03-15 13:15:46 -0400

    adding in templates

--- magic/card_search/views.py
+++ magic/card_search/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Card
+class Dashboard(ListView):
+    model = Card
+    template_name = 'blogapp/postlist.html'
+    paginate_by = 5
 
-# Create your views here.
+    def get_queryset(self):
+        return Card.objects.all()
