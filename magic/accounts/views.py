d3bd039353feeb8d2b59bf15c5d9abbb3f9a6158
jarthurj <jaoldaker1@gmail.com>
2026-08-23 15:43:52 -0400

    login required views are workign and cards show up when looking at deck detail

--- magic/accounts/views.py
+++ magic/accounts/views.py
@@ -2,22 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.views.generic import ListView,View,FormView
+from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
-
-
-
-# @login_required
-# def profile(request):
-#     ...
-
-# class login(View):
-#     template_name = "accounts/login.html"
-#     def get(self,request):
-#         render(request, self.template_name)
 class Dashboard(LoginRequiredMixin,View):
 
     template_name = "registration/dashboard.html"
@@ -46,6 +35,3 @@ class Register(View):
             "form": form
         })
     
-# class Logout(View):
-#     def get(self,request):
-#         redirect("search")
--- magic/card_search/urls.py
+++ magic/card_search/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import Home,Search,Results,CardDetailView
 from django.contrib import admin
 
+app_name = 'card_search'
 urlpatterns = [
     path('', Home.as_view(),name="home"),
     path('search/',Search.as_view(),name="search"),
