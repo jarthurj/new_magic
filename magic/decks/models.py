1a22ca9102467da271699ec2bdc1ae05d68172e9
jarthurj <jaoldaker1@gmail.com>
2026-08-29 16:40:19 -0400

    working on quantities for cards original way models were set up did not account for this

--- magic/decks/models.py
+++ magic/decks/models.py
@@ -2,35 +2,14 @@ from django.db import models
 from card_search.models import Card
 from django.conf import settings
 
-# class Deck(models.Model):
-#     name = models.CharField(max_length=100)
-#     card = models.ManyToManyField(Card,related_name="cards",blank=True)
-#     shared = models.BooleanField(default=False)
-#     user = models.ManyToManyField(
-#         settings.AUTH_USER_MODEL,
-#         related_name="users",
-#         blank=True,)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     source_deck = models.ForeignKey(
-#         UserDeck,
-#         on_delete=models.SET_NULL,
-#         null=True,
-#         blank=True,
-#         related_name="copies",
-#     )
-
-#     def __str__(self):
-#         return self.name
 
 class UserDeck(models.Model):
     FORMAT_CHOICES = [
-            ('standard', 'Standard'),
-            ('commander', 'Commander'),
-        ]
+        ('standard', 'Standard'),
+        ('commander', 'Commander'),
+    ]
     name = models.CharField(max_length=100)
-    card = models.ManyToManyField(Card,related_name="cards",blank=True)
+    card = models.ManyToManyField(Card, related_name="decks", through='DeckCard', blank=True)
     private = models.BooleanField(default=False)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -39,8 +18,13 @@ class UserDeck(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    format = models.CharField(max_length=100,choices=FORMAT_CHOICES,null=True,blank=True)
-    def make_public(self):
-        pass
+    format = models.CharField(max_length=100, choices=FORMAT_CHOICES, null=True, blank=True)
+
     def __str__(self):
         return self.name
+
+
+class DeckCard(models.Model):
+    deck = models.ForeignKey(UserDeck, on_delete=models.CASCADE)
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
